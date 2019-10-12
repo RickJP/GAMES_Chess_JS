b47ffb56c2ecda7226b807903947bbf4ea048e9a
Rick <{ID}+{username}@users.noreply.github.com>
2019-10-12 21:49:55 +0900

    P4 - File & Ranks Arrays

--- js/defs.js
+++ js/defs.js
@@ -6,16 +6,31 @@ let BRD_SQ_NUM = 120;
 let FILES = {FILE_A:0, FILE_B:1, FILE_C:2, FILE_D:3, 
              FILE_E:4, FILE_F:5, FILE_G:6, FILE_H:7, FILE_NONE:8};
 
-let RANKS = {RANK_A:0, RANK_B:1, RANK_C:2, RANK_D:3, 
-            RANK_E:4, RANK_F:5, RANK_G:6, RANK_H:7, RANK_NONE:8};
+let RANKS = {RANK_1:0, RANK_2:1, RANK_3:2, RANK_4:3, 
+            RANK_5:4, RANK_6:5, RANK_7:6, RANK_8:7, RANK_NONE:8};
 
 let COLORS = {WHITE:0, BLACK:1, BOTH:2};
 
-var SQUARES = {
+let SQUARES = {
   A1:21, B1:22, C1:23, D1:24, E1:25, F1:26, G1:27, H1:28, 
   A8:91, B8:92, C8:93, D8:94, E8:95, F8:96, G8:97, H8:98, 
   NO_SQ:99, OFF_BOARD:100
 };
 
-var BOOL = { FALSE:0, TRUE: 1};
+let BOOL = { FALSE:0, TRUE: 1};
+
+let FilesBrd = new Array(BRD_SQ_NUM);
+let RanksBrd = new Array(BRD_SQ_NUM);
+
+function FR2SQ(f, r) {
+  return ( (21 + (f) ) + (r) * 10 );
+}
+
+
+
+
+
+
+
+
 
--- js/main.js
+++ js/main.js
@@ -3,6 +3,32 @@ $(function() {
   console.log('Main Init called');
 });
 
+function InitFilesRankBrd() {
+  let index = 0;
+  let files = FILES.FILE_A;
+  let rank = RANKS.RANK_1;
+  let sq = SQUARES.A1;
+
+  for (index = 0; index < BRD_SQ_NUM; ++index) {
+    FilesBrd[index] = SQUARES.OFF_BOARD;
+    RanksBrd[index] = SQUARES.OFF_BOARD;
+  }
+
+  for (rank = RANKS.RANK_1; rank <= RANKS.RANK_8; ++rank) {
+    for (file = FILES.FILE_A; file <= FILES.FILE_H; ++file) {
+      sq = FR2SQ(file, rank);
+      FilesBrd[sq] = file;
+      RanksBrd[sq] = rank;
+    }
+  }
+
+  console.log('FilesBrd[0]: ' + FilesBrd[0] + "RanksBrd[0]: " + RanksBrd[0]);
+  console.log('FilesBrd[SQUARES.A1]: ' + FilesBrd[SQUARES.A1] + "RanksBrd[SQUARES.A1]: " + RanksBrd[SQUARES.A1]);
+  console.log('FilesBrd[SQUARES.E8]: ' + FilesBrd[SQUARES.E8] + "RanksBrd[SQUARES.E8]: " + RanksBrd[SQUARES.E8]);
+
+}
+
 function init() {
   console.log('Init() called');
+ InitFilesRankBrd();
 }
